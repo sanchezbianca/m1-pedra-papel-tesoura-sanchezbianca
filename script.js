43837294b99417bc268a0d34871a7dbeb4edc754
Bianca Sanchez <bia.sanchez62@gmail.com>
2024-11-22 17:54:43 -0300

    Lógica do vencedor criada

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+  if (
+    (player1 === "Pedra" && player2 === "Tesoura") ||
+    (player1 === "Tesoura" && player2 === "Papel") ||
+    (player1 === "Papel" && player2 === "Pedra")
+  ) {
+    return "Jogador 1 venceu!";
+  } else if (player1 === player2) {
+    return "Empate!";
+  } else {
+    return "Jogador 2 venceu!";
+  }
+}
